fe2a5a2742a0537525ebeaa7b4ad83550fd1367e
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-21 01:05:56 +0300

    Задача_41

---
+++ Tema_3_HW_6/Tema_3_HW_5_Primer_41/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+
+вводим через один Enter (разбиваем через Split())
+*/
+
+// Преобразуем числа в массив
+int [] GreatArray()
+{
+Console.WriteLine("Введите массив из M чисел, разделенных пробелами, запятыми или точками");
+string text = Console.ReadLine()!;
+char[] spearator = new char[] { ',','.',' '};
+string[] splitArray = text.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+int count =  splitArray.Length;
+int n = count;
+int[] arr = new int[n];
+int resultNumber = 0;
+
+for (int i = 0; i < count; i++)
+{
+  if (int.TryParse(splitArray[i], out resultNumber))
+  {
+    arr[i] = resultNumber;
+  }
+  else
+  {
+    Console.WriteLine("Ввели не число или не корректное число. Повторите ввод!");
+      }
+      }
+      return arr;
+}
+
+      
+// Печатаем массив
+void PrintArray (int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+     Console.Write($"{arr[i]}, ");   
+    }
+}
+
+
+int PositiveArr (int [] array)
+{
+    int k=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            k=k+1;
+        }
+    }
+
+    return k;
+}
+
+int [] b = GreatArray();
+PrintArray(b);
+int posArr = PositiveArr (b);
+Console.WriteLine($"Пользователь ввел {posArr} чисел больше нуля");
